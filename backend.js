af17f48a1d31baf15fb0dde5b5d105acbf37248d
dfel08 <dfellows68@gmail.com>
2020-04-17 14:52:33 -0700

    Fix PORT

--- backend.js
+++ backend.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.port || 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
